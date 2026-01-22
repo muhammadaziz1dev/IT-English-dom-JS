41ccd577cdbce5aeb1b43ee97dfae0cde938d938
muhammadaziz1dev <mamirjonovmuhammadaziz6@gmail.com>
2026-01-22 15:09:41 +0500

    2- dars

---
+++ js/app.js
@@ -0,0 +1,62 @@
+// let ism = 'Muhammadaziz';
+// let familiya = 'Mamirjonov';
+// let ismFailiya = ism + ' ' + familiya;
+// let email = ' mamirjonov@gmail.com '
+
+
+// console.log (ismFailiya.length)
+
+// let ism = 'Kamron'.toUpperCase()
+// let ism = 'Kamron'.toLowerCase()
+// console.log (ism)
+
+// console.log (email.indexOf('a'))
+// console.log (email.lastIndexOf('a'))
+// console.log (email.substring(10 , 8))
+// console.log (email.replace('@', '!'))
+// console.log(email.trim())
+// console.log(email.split())
+
+// let son1 = 100;
+// let son2 = 19;
+
+// console.log(son1 % son2);
+
+// son1 = 5;
+// console.log(son1 ** 2)
+
+// console.log(12 + (3+5) * 2)
+
+// let likes = 10;
+// console.log (++likes)
+
+// let likes = 10;
+// console.log (likes /= 2)
+
+// console.log (11 * 'Muhammadaziz') //NAN
+// console.log (11 + 'Muhammadaziz')
+
+// let title = 'Dubay Uy lari'
+// let narx = '150.000$'
+// let theEnd = 'ga sotib olish mumkin'
+
+// console.log('salom' + ' ' + title + ' ' + 'narxi' + ' ' + narx + theEnd)
+
+// let ism1 = Kamron
+// let ism2 = Muhammad
+// let ims3 = Boyazid
+
+// let ismlar = ['Kamron' , 'Muhammad' , 'Boyazid']
+// console.log (ismlar)
+
+// ismlar.push('Aliy')
+// console.log (ismlar)
+
+// ismlar.pop
+// console.log(ismlar)
+
+// let numbir = 25
+// console.log( numbir != 25)
+
+// let numbir2 = 25
+// console.log( numbir <= 25)
