6e6686fe8db76284a7de575828fb706632ac1fef
muhammadaziz1dev <mamirjonovmuhammadaziz6@gmail.com>
2026-02-03 13:58:09 +0500

    2-bolim

---
+++ js/bolim2.js
@@ -0,0 +1,35 @@
+// let admin = 'admin1'
+// let passWord = 12345
+
+// if (admin === 'admin1' && passWord === 12345){
+//     console.log("Parol va Login to'g'ri")
+// } else if (admin !='admin1'){
+//     console.log("Login xato")
+// } else if (passWord !== 12345){
+//     console.log("Parol xato")
+// }else{
+//     console.log("Parol yoki login xato")
+// }
+// let input = prompt(
+//     "Ma'lumotlarni kiriting:\nlogin,parol,student,teacher\nMasalan:admin,1234,yes,no"
+// )
+
+// let data = input.split(',')
+
+// let login = data[0]
+// let passWord = data[1]
+// let stu = data[2]
+// let tech = data[3]
+
+// if (login === "admin01" && passWord === "240508"){
+//     console.log("Parol va Login to'g'ri")
+
+//     if (stu === 'yes' || tech === 'yes'){
+//       console.log("Sizga chegirma bor")
+//     } else{
+//     console.log("Sizga chegirma yo'q")
+//     }
+
+// } else{
+//     console.log('Parol yoki Login xato!!!')
+// }
