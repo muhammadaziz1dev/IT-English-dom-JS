3cd1b4311a91262b8dfae184366f171698793d62
muhammadaziz1dev <mamirjonovmuhammadaziz6@gmail.com>
2026-02-23 19:08:08 +0500

    bo'lim yangi

---
+++ js/bolim4.js
@@ -0,0 +1,50 @@
+// Object 4-bo'lim
+
+// const user = {
+//     ism: "Muhammadaziz",
+//     age: 19,
+//     addres: 'Andijon shaxar',
+//     phone: 937600035,
+//     lang: ['ingliz', 'russian', 'uzbek'],
+    
+//     login: function(){
+//         console.log('Siz kirdingiz.')
+//     },
+
+//     til: function(){
+//         console.log('Barcha tillar:')
+//         this.lang.forEach((lang) =>{
+//             const natija = lang
+//             console.log(natija)
+//         })
+//     }
+// }
+// console.log(user.til())
+
+// const users = [
+//     {name: 'Behruz', age: 14, addres: 'Andijon shahar'},
+//     {name: 'Mubina', age: 16, addres: 'Andijon Tuman'},
+//     {name: 'Odina', age: 17, addres: 'Marhamad'},
+//     {name: 'Odiljon', age: 19, addres: 'Toshkent shahar'},
+//     {name: 'Valijon', age: 12, addres: 'Andijon shahar'},
+// ]
+
+// console.log("Barcha foydalanuvchilar ro'yxari")
+// users.forEach((user) =>{
+//     const natija = `Ism:${user.name}, Yoshi: ${user.age}, Manzili: ${user.addres}`
+//     console.log(natija)
+// })
+
+// let sonNone = 7.6
+// console.log(Math.round(sonNone))
+
+// sonNone = 7.3
+// console.log(Math.floor(sonNone))
+
+// sonNone = 10.9
+// console.log(Math.floor(sonNone))
+
+// sonNone = 1
+// const randomSon = (Math.random(sonNone) * 10) + 1
+// console.log(Math.trunc(randomSon))
+ 
